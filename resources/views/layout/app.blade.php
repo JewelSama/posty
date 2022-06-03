ce156c9cd9f67529a0985f331f6ea1ee770f5f2e
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-06-03 15:29:22 +0100

    RrgisterController

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,9 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        laravel: "#ef3b2d",
+                    },
+                },
+            },
+        };
+    </script>
     <title>Poty</title>
 </head>
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex item-center">
+            <li class="p-3">
+                <a href="">Home</a>
+            </li>
+            <li class="p-3">
+                <a href="">Dashboard</a>
+            </li>
+            <li class="p-3">
+                <a href="">Post</a>
+            </li>
+        </ul>
+        <ul class="flex item-center">
+            <li class="p-3">
+                <a href="">Jewel Sama</a>
+            </li>
+            <li class="p-3">
+                <a href="">Login</a>
+            </li>
+            <li class="p-3">
+                <a href="">Register</a>
+            </li>
+            <li class="p-3">
+                <a href="">Logout</a>
+            </li>
+        </ul>
+    </nav>
         @yield('content')
 </body>
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layout.app')
 
 @section('content')
-    Imdex
+    <div class="flex justify-center">
+        <div class=" w-8/12 bg-white p-6 rounded-lg">
+            Posts
+        </div>
+    </div>
 
 @endsection
