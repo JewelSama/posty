e8d9583b09c9c1c7e4fb31b9cefa42fba80f8b43
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-06-04 18:39:37 +0100

    created_at->

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -26,7 +26,7 @@
                 @foreach($posts as $post)
                     <div class="mb-4">
                         <a href="" class="font-bold">{{$post->user->username}}</a>
-                        <span class="text-sm">date</span>
+                        <span class="text-sm">{{$post->created_at->diffForHumans()}}</span>
                         <p class="mb-2">{{$post->body}}</p>
                     </div>
                 @endforeach
