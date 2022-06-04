b7409a45139e5013234f4ce7b1abd493cab98782
JewelKahz <67759825+JewelKahz@users.noreply.github.com>
2022-06-04 22:02:19 +0100

    post component

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -25,7 +25,7 @@
             @if($posts->count())
                 @foreach($posts as $post)
                     <div class="mb-4">
-                        <a href="" class="font-bold">{{$post->user->username}}</a>
+                        <a href="{{route('users.posts', $post->user)}}" class="font-bold">{{$post->user->username}}</a>
                         <span class="text-sm">{{$post->created_at->diffForHumans()}}</span>
                         <p class="mb-2">{{$post->body}}</p>
 
